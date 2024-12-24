0516fd7dcbc3f02c5abe23506d0f1759246f7d37
zbayle <126003179+zbayle@users.noreply.github.com>
2024-12-24 10:51:18 -0700

    Create tampermonkey-keyword-highlighter.user.js
    Initial commit

---
+++ tampermonkey-keyword-highlighter.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Keyword Highlighter with Floating Menu
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Highlight specified keywords dynamically with custom colors using a floating menu in Tampermonkey.
+// @author       Your Name
+// @match        *://*/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/zbayle/tampermonkey-keyword-highlighter/main/tampermonkey-keyword-highlighter.user.js
+// @downloadURL  https://raw.githubusercontent.com/zbayle/tampermonkey-keyword-highlighter/main/tampermonkey-keyword-highlighter.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Script content goes here (refer to the earlier script shared)
+})();
