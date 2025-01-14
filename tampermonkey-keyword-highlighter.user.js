d43eac01de27679f376383ecb1776d8ee0d4d37d
zbayle <126003179+zbayle@users.noreply.github.com>
2025-01-14 15:35:20 -0700

    2.0.0.1 ROC Tools

--- tampermonkey-keyword-highlighter.user.js
+++ tampermonkey-keyword-highlighter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ROC Tools with Floating Menu
 // @namespace    http://tampermonkey.net/
-// @version      1.0.10.0
+// @version      2.0.0.2
 // @description  Highlight specified keywords dynamically with custom colors using a floating menu in Tampermonkey. Also alerts when a WIM is offered on specific pages.
 // @autor        zbbayle
 // @match        https://optimus-internal.amazon.com/*
