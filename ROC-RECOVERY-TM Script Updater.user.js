07c0299f7bb5feb9fbe47a8713b9dec01b9f3bfd
zbayle <126003179+zbayle@users.noreply.github.com>
2025-01-22 13:16:33 -0700

    1.2.1.5 ROC-RECOVERY-TM Script Updater

--- ROC-RECOVERY-TM Script Updater.user.js
+++ ROC-RECOVERY-TM Script Updater.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ROC-RECOVERY-TM Script Updater 
 // @namespace    http://tampermonkey.net/
-// @version      1.2.1.4
+// @version      1.2.1.5
 // @updateURL    https://github.com/zbayle/ROC-RECOVERY-TM/raw/refs/heads/main/ROC-RECOVERY-TM%20Script%20Updater.user.js
 // @downloadURL  https://github.com/zbayle/ROC-RECOVERY-TM/raw/refs/heads/main/ROC-RECOVERY-TM%20Script%20Updater.user.js
 // @description  Automatically updates scripts from the ROC-RECOVERY-TM GitHub repository.
