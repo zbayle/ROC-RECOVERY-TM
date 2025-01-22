6bd4073091c0fc632c3c06a4d491f298400f03ee
zbayle <126003179+zbayle@users.noreply.github.com>
2025-01-22 12:21:17 -0700

    1.2.1.4 updater

--- ROC-RECOVERY-TM Script Updater.user.js
+++ ROC-RECOVERY-TM Script Updater.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ROC-RECOVERY-TM Script Updater 
 // @namespace    http://tampermonkey.net/
-// @version      1.2.1.3
+// @version      1.2.1.4
 // @updateURL    https://github.com/zbayle/ROC-RECOVERY-TM/raw/refs/heads/main/ROC-RECOVERY-TM%20Script%20Updater.user.js
 // @downloadURL  https://github.com/zbayle/ROC-RECOVERY-TM/raw/refs/heads/main/ROC-RECOVERY-TM%20Script%20Updater.user.js
 // @description  Automatically updates scripts from the ROC-RECOVERY-TM GitHub repository.
@@ -13,7 +13,7 @@
 // @grant        GM_log
 // @grant        GM_registerMenuCommand
 // @grant        GM_info
-// @connect     raw.githubusercontent.com 
+// @connect      raw.githubusercontent.com 
 // ==/UserScript==
 
 (function () {
@@ -23,20 +23,20 @@
     const scripts = [
         {
             name: "Display Hover Box Data with Time and Packages",
-            url: "https://raw.githubusercontent.com/zbayle/ROC-RECOVERY-TM/main/Display%20Hover%20Box%20Data%20with%20Time%20and%20Packages.user.js", // Updated URL
-            currentVersion: GM_getValue("Display Hover Box Version", "0.0"),
-            match: "https://trans-logistics.amazon.com/sortcenter/vista/*"  // Add the match URL pattern
+            url: "https://raw.githubusercontent.com/zbayle/ROC-RECOVERY-TM/main/Display%20Hover%20Box%20Data%20with%20Time%20and%20Packages.user.js",
+            key: "Display Hover Box Version",
+            match: "https://trans-logistics.amazon.com/sortcenter/vista/*"
         },
         {
             name: "Vista Auto Fill with VRID Scroll, Enter, and Hover",
-            url: "https://raw.githubusercontent.com/zbayle/ROC-RECOVERY-TM/main/Vista%20auto%20fill%20with%20VRID%20scroll,%20Enter,%20and%20Hover.user.js", // Updated URL
-            currentVersion: GM_getValue("Vista Auto Fill Version", "0.0"),
-            match: "https://trans-logistics.amazon.com/sortcenter/vista/*"  // Add the match URL pattern
+            url: "https://raw.githubusercontent.com/zbayle/ROC-RECOVERY-TM/main/Vista%20auto%20fill%20with%20VRID%20scroll,%20Enter,%20and%20Hover.user.js",
+            key: "Vista Auto Fill Version",
+            match: "https://trans-logistics.amazon.com/sortcenter/vista/*"
         },
         {
             name: "WIMS and FMC Interaction",
-            url: "https://raw.githubusercontent.com/zbayle/ROC-RECOVERY-TM/main/WIMS%20and%20FMC%20Interaction.user.js", // Updated URL
-            currentVersion: GM_getValue("WIMS and FMC Version", "0.0"),
+            url: "https://raw.githubusercontent.com/zbayle/ROC-RECOVERY-TM/main/WIMS%20and%20FMC%20Interaction.user.js",
+            key: "WIMS and FMC Version",
             match: [
                 "https://optimus-internal.amazon.com/wims*", 
                 "https://trans-logistics.amazon.com/fmc/execution/*",
@@ -46,7 +46,7 @@
         {
             name: "ROC Tools with Floating Menu",
             url: "https://raw.githubusercontent.com/zbayle/ROC-RECOVERY-TM/main/tampermonkey-keyword-highlighter.user.js",
-            currentVersion: GM_getValue("Keyword Highlighter Version", "0.0"),
+            key: "ROC Tools with Floating Menu Version",
             match: [
                 "https://trans-logistics.amazon.com/*",
                 "https://optimus-internal.amazon.com/*"
@@ -74,7 +74,7 @@
 
     // Update and install a script
     function updateScript(script, latestVersion, content) {
-        GM_setValue(script.name + " Version", latestVersion);
+        GM_setValue(script.key, latestVersion);
         injectScript(content); // Inject the new script into the page
         // alert(`${script.name} has been updated to version ${latestVersion}`);
     }
@@ -90,8 +90,8 @@
                         url: script.url,
                         onload: function (response) {
                             const remoteVersion = /@version\s+([\d.]+)/.exec(response.responseText)?.[1];
-                            if (remoteVersion && isNewVersion(script.currentVersion, remoteVersion)) {
-                                console.log(`Updating ${script.name} from version ${script.currentVersion} to ${remoteVersion}`);
+                            if (remoteVersion && isNewVersion(GM_getValue(script.key, "0.0"), remoteVersion)) {
+                                console.log(`Updating ${script.name} from version ${GM_getValue(script.key, "0.0")} to ${remoteVersion}`);
                                 updateScript(script, remoteVersion, response.responseText);
                             } else {
                                 console.log(`${script.name} is already up to date.`);
@@ -108,8 +108,8 @@
                     url: script.url,
                     onload: function (response) {
                         const remoteVersion = /@version\s+([\d.]+)/.exec(response.responseText)?.[1];
-                        if (remoteVersion && isNewVersion(script.currentVersion, remoteVersion)) {
-                            console.log(`Updating ${script.name} from version ${script.currentVersion} to ${remoteVersion}`);
+                        if (remoteVersion && isNewVersion(GM_getValue(script.key, "0.0"), remoteVersion)) {
+                            console.log(`Updating ${script.name} from version ${GM_getValue(script.key, "0.0")} to ${remoteVersion}`);
                             updateScript(script, remoteVersion, response.responseText);
                         } else {
                             console.log(`${script.name} is already up to date.`);
