e0ec0f937941bd669e21331807b3465cfde9b27c
zbayle <126003179+zbayle@users.noreply.github.com>
2025-02-02 10:26:35 -0700

    1.2.2.8 ROC-RECOVERY-TM Script Updater

--- ROC-RECOVERY-TM Script Updater.user.js
+++ ROC-RECOVERY-TM Script Updater.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ROC-RECOVERY-TM Script Updater 
 // @namespace    http://tampermonkey.net/
-// @version      1.2.2.7
+// @version      1.2.2.8
 // @updateURL    https://github.com/zbayle/ROC-RECOVERY-TM/raw/refs/heads/main/ROC-RECOVERY-TM%20Script%20Updater.user.js
 // @downloadURL  https://github.com/zbayle/ROC-RECOVERY-TM/raw/refs/heads/main/ROC-RECOVERY-TM%20Script%20Updater.user.js
 // @description  Automatically updates scripts from the ROC-RECOVERY-TM GitHub repository.
@@ -26,19 +26,20 @@
             name: "Display Hover Box Data with Time and Packages",
             url: "https://raw.githubusercontent.com/zbayle/ROC-RECOVERY-TM/main/Display%20Hover%20Box%20Data%20with%20Time%20and%20Packages.user.js",
             key: "Display Hover Box Version",
-            match: ['https://optimus-internal.amazon.com/*']
+            match: ['https://trans-logistics.amazon.com/sortcenter/vista/*']
         },
         {
             name: "Vista Auto Fill with VRID Scroll, Enter, and Hover",
             url: "https://raw.githubusercontent.com/zbayle/ROC-RECOVERY-TM/main/Vista%20auto%20fill%20with%20VRID%20scroll,%20Enter,%20and%20Hover.user.js",
             key: "Vista Auto Fill Version",
-            match: ['https://trans-logistics.amazon.com/*']
+            match: ['https://trans-logistics.amazon.com/sortcenter/vista/*']
+
         },
         {
             name: "WIMS and FMC Interaction",
             url: "https://raw.githubusercontent.com/zbayle/ROC-RECOVERY-TM/main/WIMS%20and%20FMC%20Interaction.user.js",
             key: "WIMS and FMC Version",
-            match: ['https://optimus-internal.amazon.com/*', 'https://trans-logistics.amazon.com/*']
+            match: ['https://optimus-internal.amazon.com/wims*', 'https://trans-logistics.amazon.com/fmc/execution/*', 'https://trans-logistics.amazon.com/sortcenter/vista/*']
         },
         {
             name: "ROC Tools with Floating Menu",
