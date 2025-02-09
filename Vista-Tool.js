1a46f9698bb43276de15dda45542a9306533acc7
zbayle <126003179+zbayle@users.noreply.github.com>
2025-02-09 14:26:30 -0700

    1.12.1  Vista-Tool

--- Vista-Tool.js
+++ Vista-Tool.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vista-Tool
-// @namespace    http://tampermonkey.net/
-// @version      1.12.0
+// @namespace    http://tampermonkey.net/  
+// @version      1.12.1
 // @updateURL    https://github.com/zbbayle/ROC-RECOVERY-TM/raw/refs/heads/main/Vista-Tool.js
 // @downloadURL  https://github.com/zbayle/ROC-RECOVERY-TM/raw/refs/heads/main/Vista-Tool.js
 // @description  Combines the functionality of displaying hover box data with time and packages and auto-filling VRID with scroll, enter, and hover, and stores the time and date of the entry that reaches 300 packages in local storage.
@@ -20,6 +20,9 @@
             console.log('Attempting to retrieve data from iframe...');
             const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
             console.log('Iframe document accessed:', iframeDocument);
+
+            // Set the facility in the iframe document
+            selectFacility(iframeDocument);
 
             // Simulate mouse over event on .progressbarib
             simulateMouseOver(iframeDocument.querySelector('.progressbarib'));
