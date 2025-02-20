dd257726949a97b991fb4d20e87631824adf48a1
zbayle <126003179+zbayle@users.noreply.github.com>
2025-02-20 16:05:47 -0700

    2.3.1 WIMS and FMC Interaction - push to main

--- WIMS and FMC Interaction.user.js
+++ WIMS and FMC Interaction.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WIMS and FMC Interaction
 // @namespace    http://tampermonkey.net/
-// @version      2.3.1.dev
+// @version      2.3.1
 // @updateURL    https://github.com/zbayle/ROC-RECOVERY-TM/raw/refs/heads/dev/WIMS and FMC Interaction.user.js
 // @downloadURL  https://github.com/zbayle/ROC-RECOVERY-TM/raw/refs/heads/dev/WIMS and FMC Interaction.user.js
 // @description  Enhanced script for WIMS and FMC with refresh timers, table redesign, toggle switches, and ITR BY integration.
